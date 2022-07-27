1e5eeecfa52636a493d77ef329162bc8c8c31ac5
Cairo Filho <cairoosf@gmail.com>
2022-07-27 10:33:24 -0300

    Ttratamento de loop e armazenamento das histórias

--- src/App/HappyNumber.php
+++ src/App/HappyNumber.php
@@ -2,63 +2,49 @@
 
 namespace App;
 
+use SebastianBergmann\Environment\Console;
+
 class HappyNumber
 {
     public array $histories = [];
     public array $digits;
-    public int $happyNumber;
 
 
-    public function isHappyNumber(): bool
+    public function isHappyNumber(int $value): bool
     {
         $status = false;
-        do {
-            $total = 0;
-            error_log(json_encode($this->getHistories()));
-            if(array_search($this->getHappyNumber(),$this->getHistories()) != null){
-                break;
-            }
-            $digits = array_map('intval', str_split($this->getHappyNumber()));
-            foreach ($digits as $item => $value) {
-                $total += ($value * $value);
-            }
-            
-            error_log($total);
-
-            
-            $this->addHistory($total);
-            $this->setHappyNumber($total);
-            if ($total == 1) {
-                $status = true;
-                break;
-            }
-            unset($total);
-            
-        } while ($this->getHappyNumber() != 1);
+        while ($value != 1 && $this->isRepeated($value) == false) {
+            $value = $this->getSunNumbers($value);
+        }
+        ($value != 1) ?: $status = true;
         return $status;
+    }
+
+    public function getSunNumbers(int $number): int
+    {
+        $total = 0;
+        $digits = array_map('intval', str_split($number));
+        foreach ($digits as $item => $value) {
+            $total += ($value * $value);
+        }
+        $this->addHistory($number);
+        return $total;
+    }
+
+    public function isRepeated($number): bool
+    {
+        if (array_search($number, $this->getHistories()) != null) {
+            return true;
+        }
+        return false;
     }
 
     public function addHistory($value): void
     {
         $this->histories[] = $value;
     }
-
-    public function getHappyNumber(): int
-    {
-        return $this->happyNumber;
-    }
     public function getHistories(): array
     {
         return $this->histories;
-    }
-
-    public function setDigits(array $value): void
-    {
-        $this->digits = $value;
-    }
-
-    public function setHappyNumber(int $value): void
-    {
-        $this->happyNumber = $value;
     }
 }
